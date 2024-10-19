eef05aea66d116cc0cbb06b504e0266daf21b4fd
Amol M <117634099+Amol5766@users.noreply.github.com>
2024-10-19 20:07:20 +0530

    Update Treatments.jsx

--- src/components/Treatments.jsx
+++ src/components/Treatments.jsx
@@ -18,20 +18,20 @@ import { FaArrowRight } from 'react-icons/fa'; // Import the animated arrow icon
 
 const Treatments = () => {
   const treatments = [
-    { id: 1, title: "Cataract Surgery", description: "Cataract surgery is a common procedure to remove cloudy lenses in the eye caused by cataracts.", image: cataractsImage, link: "/cataract-surgery" },
-    { id: 2, title: "Refractive Surgery", description: "Refractive surgery is the process of reclaiming your vision and freedom. After careful evaluation, our experts can decrease or completely eliminate your dependency on glasses.", image: glaucomaImage, link: "/retina-services" },
-    { id: 3, title: "Retina Services", description: "The retina is the light-sensitive layer of tissue at the back of your eyeball. At Vasan, we employ advanced techniques to diagnose diseases that affect your retina.", image: macularDegenerationImage, link: "/corneal-services" },
-    { id: 4, title: "Corneal Services", description: "Various conditions can impact the cornea, potentially causing vision issues. Corneal services is a collective term referring to treatments to fix these issues.", image: diabeticRetinopathyImage, link: "/Anti-VEGF-Agents" },
-    { id: 5, title: "Anti-VEGF Agents", description: "Anti-VEGF agents are a group of medications which block the activity of VEGF, therefore reducing bleeding and vascular leakage.", image: dryEyeImage, link: "/dry-eye-treatment" },
-    { id: 6, title: "Dry Eye Treatment", description: "Dry eye syndrome happens when there's a change in the quality or amount of the three layers that make up your tear film.", image: amblyopiaImage, link: "/vitrectomy" },
-    { id: 7, title: "Vitrectomy", description: "Vitrectomy is an advanced surgery done by our specialists to remove the gel-like substance called ‘vitreous humour’ from inside your eye. This helps them get better access to your retina.", image: conjunctivitisImage, link: "/contoura-vision" },
-    { id: 8, title: "Contoura Vision", description: "Contoura Vision is a groundbreaking laser vision correction procedure that enables you to correct your vision for a lifetime.", image: keratoconusImage, link: "/ICL" },
-    { id: 9, title: "ICL", description: "EVO ICL, or Implantable Collamer Lens, is a refractive procedure that corrects myopia (near-sightedness).", image: uveitisImage, link: "/PRK" },
-    { id: 10, title: "PRK", description: "Photorefractive keratectomy (PRK) is a refractive laser surgery that reshapes the cornea in your eyes to correct myopia and hyperopia.", image: retinalDetachmentImage, link: "/Epi-LASIK" },
-    { id: 11, title: "Epi-LASIK", description: "Epi-LASIK is a modern refractive treatment option to enable clear vision without glasses or contact lenses.", image: retinitisPigmentosaImage, link: "/Epi-LASIK" },
-    { id: 12, title: "Epi-Contoura", description: "The newest advancement in laser vision correction for eliminating the need for glasses is Epi-Contoura Streamlight.", image: floatersImage, link: "/Epi-Contoura" },
-    { id: 13, title: "Retinal Laser Photocoagulation", description: "Retinal laser photocoagulation is a treatment we use at Vasan to address several retina-related issues such as diabetic retinopathy, retinal vein occlusion, and others.", image: blepharitisImage, link: "/retinal-laser-photocoagulation" },
-    { id: 14, title: "Customised LASIK", description: "Customised LASIK is an advanced procedure that helps you see perfectly without glasses or contact lenses.", image: Customisedlasik, link: "/Customised-lasik" },
+    { id: 1, title: "Cataract Surgery", description: "Cataract surgery is a common procedure to remove cloudy lenses in the eye caused by cataracts.", image: cataractsImage, link: "" },
+    { id: 2, title: "Refractive Surgery", description: "Refractive surgery is the process of reclaiming your vision and freedom. After careful evaluation, our experts can decrease or completely eliminate your dependency on glasses.", image: glaucomaImage, link: "" },
+    { id: 3, title: "Retina Services", description: "The retina is the light-sensitive layer of tissue at the back of your eyeball. At Vasan, we employ advanced techniques to diagnose diseases that affect your retina.", image: macularDegenerationImage, link: "" },
+    { id: 4, title: "Corneal Services", description: "Various conditions can impact the cornea, potentially causing vision issues. Corneal services is a collective term referring to treatments to fix these issues.", image: diabeticRetinopathyImage, link: "" },
+    { id: 5, title: "Anti-VEGF Agents", description: "Anti-VEGF agents are a group of medications which block the activity of VEGF, therefore reducing bleeding and vascular leakage.", image: dryEyeImage, link: "" },
+    { id: 6, title: "Dry Eye Treatment", description: "Dry eye syndrome happens when there's a change in the quality or amount of the three layers that make up your tear film.", image: amblyopiaImage, link: "" },
+    { id: 7, title: "Vitrectomy", description: "Vitrectomy is an advanced surgery done by our specialists to remove the gel-like substance called ‘vitreous humour’ from inside your eye. This helps them get better access to your retina.", image: conjunctivitisImage, link: "" },
+    { id: 8, title: "Contoura Vision", description: "Contoura Vision is a groundbreaking laser vision correction procedure that enables you to correct your vision for a lifetime.", image: keratoconusImage, link: "" },
+    { id: 9, title: "ICL", description: "EVO ICL, or Implantable Collamer Lens, is a refractive procedure that corrects myopia (near-sightedness).", image: uveitisImage, link: "" },
+    { id: 10, title: "PRK", description: "Photorefractive keratectomy (PRK) is a refractive laser surgery that reshapes the cornea in your eyes to correct myopia and hyperopia.", image: retinalDetachmentImage, link: "" },
+    { id: 11, title: "Epi-LASIK", description: "Epi-LASIK is a modern refractive treatment option to enable clear vision without glasses or contact lenses.", image: retinitisPigmentosaImage, link: "" },
+    { id: 12, title: "Epi-Contoura", description: "The newest advancement in laser vision correction for eliminating the need for glasses is Epi-Contoura Streamlight.", image: floatersImage, link: "" },
+    { id: 13, title: "Retinal Laser Photocoagulation", description: "Retinal laser photocoagulation is a treatment we use at Vasan to address several retina-related issues such as diabetic retinopathy, retinal vein occlusion, and others.", image: blepharitisImage, link: "" },
+    { id: 14, title: "Customised LASIK", description: "Customised LASIK is an advanced procedure that helps you see perfectly without glasses or contact lenses.", image: Customisedlasik, link: "" },
   ];
 
   const [headerVisible, setHeaderVisible] = useState(false);
