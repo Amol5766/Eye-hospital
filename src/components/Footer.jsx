10da97dae20314afa7651f403033b02d0af33eb7
Amol5766 <amolmaheshwara270@gmail.com>
2026-01-04 00:16:12 +0530

    address updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,14 +6,21 @@ const Footer = () => {
       <div className="flex flex-col md:flex-row justify-between p-8 md:px-32 px-5">
         <div className="w-full md:w-1/4">
           <h1 className="text-2xl font-semibold">
-            <span className="inline-block transform font-bold italic -translate-x-5">Nanjappa</span>
-            <span className="block text-2xl mt-1 ml-1 font-bold italic -translate-y-2" style={{ color: "#EB3535" }}>
+            <span className="inline-block transform font-bold italic -translate-x-5">
+              Nanjappa
+            </span>
+            <span
+              className="block text-2xl mt-1 ml-1 font-bold italic -translate-y-2"
+              style={{ color: '#EB3535' }}
+            >
               Eye Hospital
             </span>
           </h1>
           <p className="text-sm mt-4">
-            At Nanjappa Eye Hospital, we are dedicated to providing the highest standard of eye care services to our patients. 
-            Our mission is to protect and enhance vision through exceptional medical expertise, cutting-edge technology, and compassionate care.
+            At Nanjappa Eye Hospital, we are dedicated to providing the highest
+            standard of eye care services to our patients. Our mission is to
+            protect and enhance vision through exceptional medical expertise,
+            cutting-edge technology, and compassionate care.
           </p>
         </div>
         <div>
@@ -65,23 +72,26 @@ const Footer = () => {
         <div className="w-full md:w-1/4">
           <h1 className="font-medium text-xl pb-4 pt-5 md:pt-0">Contact Us</h1>
           <nav className="flex flex-col gap-2">
-            <a href="https://maps.google.com/?q=Above+Corporation+Bank,+Vivekananda+road,+Kollegala,+Karnataka+571440" target="_blank" rel="noopener noreferrer">
-              Above Corporation Bank, Vivekananda road, Kollegala, Karnataka 571440
+            <a
+              href="https://maps.app.goo.gl/SrM1993fXXXeBRxY6"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-base"
+            >
+              near Medical College, Behind royal zam zam
+              convention hall, Yadapura, Chamarajanagar, Karnataka 571313
             </a>
             <a href="mailto:nanjapppaeyehospital@gmail.com">
               nanjapppaeyehospital.kollegal@gmail.com
             </a>
-            <a href="tel:+08224254330">
-              +08224254330
-            </a>
+            <a href="tel:+08224254330">+08224254330</a>
           </nav>
         </div>
       </div>
       <div>
         <p className="text-center py-4">
           @copyright developed by
-          <span className="text-hoverColor"> Amol</span> | All
-          rights reserved
+          <span className="text-hoverColor"> Amol</span> | All rights reserved
         </p>
       </div>
     </div>
